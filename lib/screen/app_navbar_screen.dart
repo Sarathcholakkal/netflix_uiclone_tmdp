9bfc319016286acff9f0328981a378964d5f895d
sarath <ssarathcholakkall@gmail>
2025-07-16 21:48:36 +0530

    home screen started

---
+++ lib/screen/app_navbar_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:netflix_uiclone/screen/netiflix_home_screen.dart';
+
+class AppNavbarScreen extends StatelessWidget {
+  const AppNavbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          height: 70,
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Iconsax.home5), text: 'Home'),
+              Tab(icon: Icon(Iconsax.search_normal), text: 'Search'),
+              Tab(icon: Icon(Icons.photo_library_outlined), text: 'Hot News'),
+            ],
+            unselectedLabelColor: Colors.grey,
+            labelColor: Colors.white,
+            indicatorColor: Colors.transparent,
+          ),
+        ),
+        body: TabBarView(
+          children: [NetiflixHomeScreen(), Scaffold(), Scaffold()],
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:http/http.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:intl/intl.dart';
+import 'package:netflix_uiclone/screen/app_navbar_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +17,18 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => AppNavbarScreen()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Center(child: Lottie.asset("assets/logo_animation.json"));
   }
 }
